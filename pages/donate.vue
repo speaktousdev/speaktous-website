b7010bd6c3d7b3d593df3ef69e4988a2cbb03702
Mujahid Anuar <mujahidfanuar@gmail.com>
2020-02-23 15:09:17 -0600

    add donate page design - Draft 1

--- pages/donate.vue
+++ pages/donate.vue
@@ -1,54 +1,111 @@
 <template>
-  <main class="p-4 text-center">
-    <h1 class="text-5xl text-center text-gray-800">Make a donation, today!</h1>
-
-    <div class="mt-4 mb-8 text-center">
-      <div class="inline-block w-48 h-48 bg-green-700 rounded-full">
-        <img src="~/assets/graphics/donate/hand.png" alt="A donating hand." />
+  <main class="p-4 lg:px-16">
+    <!-- This is for mobile screens -->
+    <div class="text-center lg:hidden">
+      <h1 class="text-4xl text-gray-800">
+        Make a donation, today!
+      </h1>
+      <div class="mt-4 mb-8 text-center">
+        <div class="inline-block w-48 h-48 bg-green-700 rounded-full">
+          <img src="~/assets/graphics/donate/hand.png" alt="A donating hand." />
+        </div>
       </div>
-    </div>
-
-    <div
-      class="p-4 text-xl text-gray-800 bg-gray-300 rounded-lg shadow-xl sm:mx-20"
-    >
-      <p class="mb-2">
-        SpeakToUs is a nonprofit, student-led organization based in Madison, WI.
-      </p>
-      <p class="mb-2">
-        The organization is managed by student volunteers who are dedicated to
-        provide mental health support for Malaysian students in the US.
-      </p>
-      <p class="mb-2">Our financial needs rely on your support.</p>
-    </div>
-
-    <div class="flex flex-col px-12 mt-10 mb-10">
-      <div class="text-center">
-        <div class="inline-block">
-          <div class="flex flex-row">
-            <p class="mb-8 mr-4 text-xl">DONATE WITH PAYPAL</p>
-            <div class="w-8 h-8">
-              <img
-                src="~/assets/graphics/donate/paypal.png"
-                alt="Paypal logo."
-              />
+      <div
+        class="p-4 mx-4 text-xl text-gray-800 bg-gray-300 rounded-lg shadow-xl sm:mx-20"
+      >
+        <p class="mb-2">
+          SpeakToUs is a nonprofit, student-led organization based in Madison,
+          WI.
+        </p>
+        <p class="mb-2">
+          The organization is managed by student volunteers who are dedicated to
+          provide mental health support for Malaysian students in the US.
+        </p>
+        <p class="mb-2">Our financial needs rely on your support.</p>
+      </div>
+      <div class="flex flex-col px-12 mt-10 mb-10">
+        <div class="text-center">
+          <div class="inline-block">
+            <div class="flex flex-row">
+              <p class="mb-8 mr-4 text-xl">DONATE WITH PAYPAL</p>
+              <div class="w-8 h-8">
+                <img
+                  src="~/assets/graphics/donate/paypal.png"
+                  alt="Paypal logo."
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div>
-        <button
-          class="justify-center px-20 py-2 text-center text-white bg-green-400 rounded hover:bg-green-700"
-        >
-          Donate
-        </button>
+        <div>
+          <button
+            class="justify-center px-20 py-2 text-center text-white bg-green-400 rounded hover:bg-green-700"
+          >
+            Donate
+          </button>
+        </div>
       </div>
     </div>
 
-    <h1 class="mt-20 text-5xl text-center text-gray-800">
+    <!-- This is for large (desktop) screens -->
+    <div class="flex flex-wrap hidden px-12 mt-10 lg:flex lg:text-left">
+      <div class="w-3/5 pr-12">
+        <h1 class="mb-10 text-5xl text-gray-800">
+          Make a donation, <br />today!
+        </h1>
+        <div class="p-6 text-xl text-gray-800 bg-gray-300 rounded-lg shadow-xl">
+          <p class="mb-4">
+            SpeakToUs is a nonprofit, student-led organization based in Madison,
+            WI.
+          </p>
+          <p class="mb-4">
+            The organization is managed by student volunteers who are dedicated
+            to provide mental health support for Malaysian students in the US.
+          </p>
+          <p class="">Our financial needs rely on your support.</p>
+        </div>
+      </div>
+
+      <div class="w-2/5">
+        <div class="my-8 text-center">
+          <div class="inline-block w-64 h-64 bg-green-700 rounded-full">
+            <img
+              src="~/assets/graphics/donate/hand.png"
+              alt="A donating hand."
+            />
+          </div>
+        </div>
+
+        <div class="flex flex-col">
+          <div class="mb-6 text-center">
+            <div class="inline-block">
+              <div class="flex flex-row">
+                <p class="mr-4 text-2xl">Donate with PAYPAL</p>
+                <div class="w-8 h-8">
+                  <img
+                    src="~/assets/graphics/donate/paypal.png"
+                    alt="Paypal logo."
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="text-center">
+            <button
+              class="inline-block px-20 py-2 text-center text-white bg-green-400 rounded hover:bg-green-700"
+            >
+              Donate
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <h1 class="mt-20 mb-8 text-4xl text-center text-gray-800 sm:text-5xl">
       With donations, we can provide:
     </h1>
-    <div class="mb-10">
-      <div class="my-10 text-center">
+    <div class="mb-10 lg:flex lg:flex-row">
+      <div class="px-4 my-10 text-center md:px-40 lg:px-10">
         <div class="inline-block w-48 h-48">
           <img
             src="~/assets/graphics/donate/dialog-box.png"
@@ -60,7 +117,7 @@
         </p>
       </div>
 
-      <div class="my-10 text-center">
+      <div class="px-4 my-10 text-center md:px-40 lg:px-10">
         <div class="inline-block w-48 h-48">
           <img src="~/assets/graphics/donate/nametag.png" alt="A nametag." />
         </div>
@@ -70,7 +127,7 @@
         </p>
       </div>
 
-      <div class="my-10 text-center">
+      <div class="px-4 my-10 text-center md:px-40 lg:px-10">
         <div class="inline-block w-48 h-48">
           <img
             src="~/assets/graphics/donate/present.png"
@@ -85,7 +142,7 @@
     </div>
 
     <div>
-      <p class="text-gray-600">
+      <p class="text-center text-gray-600">
         *Donations will be utilized but not limited only to those means.
       </p>
     </div>
@@ -96,4 +153,8 @@
 export default {}
 </script>
 
-<style></style>
+<style scoped>
+.bg-green {
+  background-color: '#74B757';
+}
+</style>
