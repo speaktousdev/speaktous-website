ab9615f782f0646d27a1d997210cde26df2b3876
Mirza Nor Azman <spellhaikarl@gmail.com>
2020-11-30 15:40:22 -0600

    Fix chat isOnline on wrong time

--- components/ScheduleTable.vue
+++ components/ScheduleTable.vue
@@ -3,10 +3,10 @@
     <thead>
       <tr>
         <th v-if="isDstObserved" class="w-1/2 px-2 py-2 border">
-          US Central Time <br />GMT-6
+          US Central Time <br />GMT-5
         </th>
         <th v-else class="w-1/2 px-2 py-2 border">
-          US Central Time <br />GMT-5
+          US Central Time <br />GMT-6
         </th>
         <th class="w-1/2 px-2 py-2 border">MY Time <br />GMT+8</th>
       </tr>
@@ -26,10 +26,10 @@
         <td class="px-2 py-2 border">
           {{ convertMalaysiaSchedule(schedule).day }} <br />
           {{ convertMalaysiaSchedule(schedule).startTime }}
-          {{ convertMalaysiaSchedule(schedule).switchStartMeridiem }}
+          {{ convertMalaysiaSchedule(schedule).startMeridiem }}
           -
           {{ convertMalaysiaSchedule(schedule).endTime }}
-          {{ convertMalaysiaSchedule(schedule).switchEndMeridiem }}
+          {{ convertMalaysiaSchedule(schedule).endMeridiem }}
         </td>
       </tr>
     </tbody>
@@ -50,7 +50,6 @@ export default {
   },
   methods: {
     convertMalaysiaSchedule(schedule) {
-      // Calculations consider Daylight saving starts
       const dayArray = [
         'Sunday',
         'Monday',
@@ -61,42 +60,45 @@ export default {
         'Saturday',
         'Sunday',
       ]
+
       let mytStartTime
-      let convertStartMeridiem = false
+      let startMeridiemString
       if (schedule.startTime >= 12) {
         mytStartTime = this.isDstObserved
-          ? schedule.startTime - 10
-          : schedule.startTime - 11
-        convertStartMeridiem = true
+          ? schedule.startTime - 11
+          : schedule.startTime - 10
+        startMeridiemString = mytStartTime >= 12 ? 'PM' : 'AM'
       } else {
         mytStartTime = this.isDstObserved
-          ? schedule.startTime + 2
-          : schedule.startTime + 1
+          ? schedule.startTime + 1
+          : schedule.startTime + 2
+        startMeridiemString = 'PM'
       }
 
       let mytEndTime
-      let convertEndMeridiem = false
+      let endMeridiemString
       if (schedule.endTime >= 12) {
         mytEndTime = this.isDstObserved
-          ? schedule.endTime - 10
-          : schedule.endTime - 11
-        convertEndMeridiem = true
+          ? schedule.endTime - 11
+          : schedule.endTime - 10
+        endMeridiemString = mytEndTime >= 12 ? 'PM' : 'AM'
       } else {
         mytEndTime = this.isDstObserved
-          ? schedule.endTime + 2
-          : schedule.endTime + 1
+          ? schedule.endTime + 1
+          : schedule.endTime + 2
+        endMeridiemString = 'PM'
       }
 
       return {
         day:
-          schedule.startTime <= 10
+          (this.isDstObserved && schedule.startTime <= 10) ||
+          (!this.isDstObserved && schedule.startTime <= 9)
             ? dayArray[dayArray.indexOf(schedule.day)]
             : dayArray[dayArray.indexOf(schedule.day) + 1],
         startTime: mytStartTime % 12 === 0 ? 12 : mytStartTime % 12,
         endTime: mytEndTime % 12 === 0 ? 12 : mytEndTime % 12,
-        switchStartMeridiem:
-          mytStartTime >= 12 && convertStartMeridiem ? 'PM' : 'AM',
-        switchEndMeridiem: mytEndTime >= 12 && convertEndMeridiem ? 'PM' : 'AM',
+        startMeridiem: startMeridiemString,
+        endMeridiem: endMeridiemString,
       }
     },
   },
