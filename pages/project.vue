4bfa08706dc8bef781c44b625fda50dbdc02aafe
Mirza Nor Azman <spellhaikarl@gmail.com>
2021-09-10 00:32:30 -0500

    Add services section

--- pages/project.vue
+++ pages/project.vue
@@ -5,12 +5,14 @@
         <h1 class="font-bold font-serif text-4xl lg:text-6xl">
           Seasonal projects
         </h1>
-        <p class="mt-4">
+        <p class="mt-12 lg:text-left">
           SpeakToUs creates seasonal projects to promote mental health
           awareness. These projects happen a limited time, so follow us in
           social media for updates on our upcoming projects this year!
         </p>
-        <p class="mt-12">Check out our past and current projects here.</p>
+        <p class="mt-8 lg:text-left">
+          Check out our past and current projects here.
+        </p>
       </div>
       <ProjectIcon class="w-1/4 ml-24 hidden md:block" />
     </section>
@@ -24,11 +26,11 @@
         <h1 class="font-bold font-serif text-2xl lg:text-4xl">
           Self Care Packages
         </h1>
-        <p class="mt-4 px-12">
+        <p class="mt-4 md:px-12 lg:text-left">
           Reward yourself with therapeutic kits to pamper yourself or surprise
           your loved ones with comforting care packages.
         </p>
-        <p class="mt-8 px-12">
+        <p class="mt-8 md:px-12 lg:text-left">
           We have launched two self-packages: Christmas and Valentine’s Day.
         </p>
       </div>
@@ -42,7 +44,7 @@
       <PodcastIcon class="md:w-5/12" />
       <div class="md:w-7/12 text-center mt-4 md:mt-16">
         <h1 class="font-bold font-serif text-2xl lg:text-4xl">Podcast</h1>
-        <p class="mt-4 px-12">
+        <p class="mt-4 md:px-12 lg:text-left">
           Listen to mental health related topics <br />
           and earn tips on how to practice self love.
         </p>
