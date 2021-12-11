0e5745f32abb83043130f8860ca2e4764bcc80c7
Riza Hassan <55040840+rizahassan@users.noreply.github.com>
2021-12-11 12:49:20 -0600

    Addedd student committee page

---
+++ components/student-committee/HeroSection.vue
@@ -0,0 +1,43 @@
+<template>
+  <main class="mx-auto max-w-7xl px-4 sm:px-6 md:px-0">
+    <div class=" flex flex-col-reverse md:flex-row">
+      <div
+        class="
+          sm:text-center
+          md:max-w-2xl
+          lg:col-span-6 lg:text-left
+        "
+      >
+        <h1 class="text-4xl md:text-4xl font-extrabold tracking-wide lg:text-6xl md:text-left">Student Committee</h1>
+        <p
+          class="
+            mt-3
+            text-base text-gray-500
+            sm:mt-5 sm:text-xl
+            md:text-base
+            lg:text-lg
+            xl:text-xl
+            md:pr-24
+          "
+        >
+          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
+          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua
+          ad ad non deserunt sunt.
+        </p>
+      </div>
+      <HeaderIcon class="w-full md:w-1/2"/>
+    </div>
+  </main>
+</template>
+<script>
+import HeaderIcon from '../../components/svg/committee/HeaderIcon.vue'
+
+export default {
+components:{ HeaderIcon }
+}
+</script>
+
+
+export default{
+  
+}
---
+++ pages/committee.vue
@@ -0,0 +1,116 @@
+<template>
+  <main
+    class="max-w-screen-xl mx-auto text-black lg:text-gray-800 px-10 font-serif">
+    <section class="md:mt-16"><HeroSection /></section>
+    <section class="my-16">
+      <div class="flex items-center justify-between">
+        <h2 class="text-2xl font-extrabold tracking-tight sm:text-4xl">
+          The Team
+        </h2>
+        <div>
+          <label for="location" class="block text-sm font-medium text-gray-700">Year</label>
+          <select
+            id="location"
+            name="location"
+            class="
+              mt-1
+              block
+              w-full
+              pl-3pr-10
+              py-2
+              text-base
+              border-gray-300
+              focus:outline-none focus:ring-indigo-500 focus:border-indigo-500
+              sm:text-sm
+              rounded-md
+            "
+          >
+            <option
+              v-for="committee in committees"
+              :key="committee.year"
+              selected
+            >
+              {{ committee.year }}
+            </option>
+          </select>
+        </div>
+      </div>
+      <StudentsCard class="my-12" :students-list="committees[0].studentsList" />
+    </section>
+    <section class="my-16">
+      <Achievements
+        class="my-12 lg:ml-12"
+        :achievementsList="committees[0].achievementsList"
+      />
+    </section>
+  </main>
+</template>
+
+<script>
+import HeroSection from '../components/student-committee/HeroSection.vue'
+import StudentsCard from '../components/student-committee/StudentsCards.vue'
+import Achievements from '../components/student-committee/Achievements.vue'
+
+export default {
+  components: { HeroSection, StudentsCard, Achievements },
+  data: () => ({
+    committees: [
+      {
+        year: '2021',
+        studentsList: [
+          {
+            name: 'Mujahid',
+            image:
+              'https://static.vecteezy.com/system/resources/previews/000/643/326/original/vector-group-people-icon.jpg',
+            role: 'President',
+            caption:
+              'Lorem ipsum dolor sit amet, conectetur adipiscing elit, sed do smod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer ulla  ',
+          },
+          {
+            name: 'Mujahid',
+            image:
+              'https://static.vecteezy.com/system/resources/previews/000/643/326/original/vector-group-people-icon.jpg',
+            role: 'President',
+            caption:
+              'Lorem ipsum dolor sit amet, conectetur adipiscing elit, sed do smod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer ulla  ',
+          },
+          {
+            name: 'Mujahid',
+            image:
+              'https://static.vecteezy.com/system/resources/previews/000/643/326/original/vector-group-people-icon.jpg',
+            role: 'President',
+            caption:
+              'Lorem ipsum dolor sit amet, conectetur adipiscing elit, sed do smod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer ulla  ',
+          },
+        ],
+        achievementsList: [
+          {
+            title: 'Mental Health Campaign SpeakToUs',
+            image:
+              'https://static.vecteezy.com/system/resources/previews/000/643/326/original/vector-group-people-icon.jpg',
+            date: 'September 2021',
+            caption:
+              'Lorem ipsum dolor sit amet, conectetur adipiscing elit, sed do smod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer ulla  ',
+          },
+          {
+            title: 'Mental Health Campaign SpeakToUs 2',
+            image:
+              'https://static.vecteezy.com/system/resources/previews/000/643/326/original/vector-group-people-icon.jpg',
+            date: 'September 2021',
+            caption:
+              'Lorem ipsum dolor sit amet, conectetur adipiscing elit, sed do smod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer ulla  ',
+          },
+          {
+            title: 'Mental Health Campaign SpeakToUs 2',
+            image:
+              'https://static.vecteezy.com/system/resources/previews/000/643/326/original/vector-group-people-icon.jpg',
+            date: 'September 2021',
+            caption:
+              'Lorem ipsum dolor sit amet, conectetur adipiscing elit, sed do smod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer ulla  ',
+          },
+        ],
+      },
+    ],
+  }),
+}
+</script>
