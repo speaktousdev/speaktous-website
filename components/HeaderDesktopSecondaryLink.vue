95185a55d8d274a5e9a12fd78130f0d9eea027b5
Mujahid Fakhrul Anuar <mujahidfanuar@gmail.com>
2021-12-12 02:15:51 +0800

    Nav link revamp - About page sublinks

---
+++ components/HeaderDesktopSecondaryLink.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<template>
+  <nuxt-link :to="to" class="-m-3 p-3 block rounded-md hover:bg-gray-50">
+    <p class="text-base font-medium text-gray-900">{{ name }}</p>
+    <!-- <p class="mt-1 text-sm text-gray-400">
+      {{ description }}
+    </p> -->
+  </nuxt-link>
+</template>
---
+++ components/HeaderMobileSecondaryLink.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<template>
+  <nuxt-link
+    :to="to"
+    class="text-base font-medium text-gray-900 hover:text-gray-700"
+    ><slot></slot
+  ></nuxt-link>
+</template>
